2742a1cff069aef077f8664e7a4c6411fa353e3c
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-05-20 13:02:04 -0300

    att

---
+++ aula012/ex011.js
@@ -0,0 +1,14 @@
+var idade = 15
+if (idade >=63) {
+    console.log('Você é maior de idade')
+    console.log('Seu voto é opcional')
+} else if (idade>=18) {
+        console.log('Você é maior de idade')
+        console.log('Você vota!')
+    } else if ((idade >=16) && (idade <18)) {
+            console.log('Você é menor de idade')
+            console.log('Seu voto é opcional!')
+        } else if (idade <16) {
+                console.log('Você é menor de idade')
+                console.log('Você náo vota!')
+            }       
