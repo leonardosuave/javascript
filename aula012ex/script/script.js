980b8594ab9f7d4d67dd086e3f26a5142ce97188
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-05-23 11:53:01 -0300

    att

---
+++ aula012ex/script/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()    
+    if (hora >=0 && hora < 12) {
+        msg.innerHTML = 'Bom dia'
+        msg.innerHTML += `<br>Agora são ${hora.toFixed(2).replace ('.',':')} horas.`        
+        img.src = 'imagens/manha-c.png'
+        window.document.body.style.background = '#D7BFA6'
+    } else if (hora >=12 && hora <19) {
+        msg.innerHTML = 'Boa tarde'
+        msg.innerHTML += `<br>Agora são ${hora.toFixed(2).replace ('.',':')} horas.`
+        img.src = 'imagens/tarde-c.png'
+        window.document.body.style.background = '#F28705'
+    } else {
+        msg.innerHTML = 'Boa noite'
+        msg.innerHTML += `<br>Agora são ${hora.toFixed(2).replace ('.',':')} horas.`
+        img.src = 'imagens/noite-c.png'
+        window.document.body.style.background = '#556573'
+    }
+}
