990bbc16765dc088a8b4e155bb9115944b94dc5c
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-05-24 11:18:51 -0300

    Create estruturawhile.js

---
+++ aula013/estruturawhile.js
@@ -0,0 +1,15 @@
+/* Estrutura de repetição com teste lógico no inicio */
+var c = 1
+while (c <=10) {
+    console.log('Olá, tudo bem?')
+    c++
+}
+
+
+/* Estrutura de rtepetição com teste lógico no final
+var c = 1   
+    do {
+        console.log('Olá, Tudo bem?')
+        c++
+    } while (c <=3) 
+    */
