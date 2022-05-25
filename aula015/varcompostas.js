efcb582eab3a968a3cc67d3a02a88feee4c0d486
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-05-25 14:17:27 -0300

    att

--- aula015/varcompostas.js
+++ aula015/varcompostas.js
@@ -3,12 +3,22 @@
 var num = [5,8,9,2]
 num[4] = 3 //método para adicionar um valor dentro de um índice da var composta pré-determinada. Pode ser utilizado para substituir valor de um índice de dentro do vetor ja estabelecido com valor. ex num[3] = 4 substituiria o ultimo índice = 2 pelo valor 4 
 num.push(7) // adiciona um indice no final com o valor 7
+
 console.log(`Nosso valor do vetor é ${num} e tem um total de ${num.length} índices`)
 console.log (`O terceiro valor do vetor é ${num[2]}`)
+console.log (`Posição do valor 8 é ${num.indexOf(8)}`) //procurar posição do valor 8
+console.log (`Posição do valor 6 é ${num.indexOf(6)}`) //procurar posição do valor 6 -- retorna -1 porque não existe este valor
 
-for(var c = 0; c<=(num.length - 1); c++) {
-    console.log(`O valor do índice é ${num[c]}`)
+//Esse método funciona para Array e Object no JavaScript 
+for (var c in num) {
+    console.log(`O valor do índice ${c} é ${num[c]}`)
 }
+
+/*
+for(var c = 0; c<=(num.length - 1); c++) {
+    console.log(`O valor do índice ${c} é ${num[c]}`)
+}
+*/
 
 /* Método com DO WHILE
 c = 0
