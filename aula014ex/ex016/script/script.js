0c8b6252824a631a9f14c0ff70902c0f8defee25
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-05-24 23:18:45 -0300

    Update script.js

--- aula014ex/ex016/script/script.js
+++ aula014ex/ex016/script/script.js
@@ -6,11 +6,11 @@ function tabuada() {
         window.alert('Por favor, digite um número!')    
     } else {
         var n = Number(num.value)
-        tab.innerHTML = ''
+        tab.innerHTML = '' //para limpar quando eu inserir um novo numero em NUN
         for (c = 1; c <= 10; c++) {
             var item = document.createElement('option')        
             item.text = ` ${n} X ${c} = ${n*c} `
-            item,value = `tab${c}` // Faz mais sentido para PHP - Vai atribuir tab para cada linha da option
+            item.value = `tab${c}` // Faz mais sentido para PHP - Vai atribuir tab para cada linha da option
             tab.appendChild(item)  // vai atribuir um filho ITEM dentro da var tab que é referente ao id tab      
         } 
     }      
