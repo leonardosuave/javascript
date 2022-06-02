584b3818ea2d4cfaf82f3518a78f8f24b8c40137
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-02 13:57:00 -0300

    att

--- imc/assets/js/main.js
+++ imc/assets/js/main.js
@@ -1,35 +1,37 @@
 // Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const inputPeso = e.target.querySelector('#peso');
-  const inputAltura = e.target.querySelector('#altura');
+form.addEventListener('submit', function (evento) {
+  evento.defaultPrevented();
+  console.log('Evento previnido')
 
-  const peso = Number(inputPeso.value);
-  const altura = Number(inputAltura.value);
+  const inputpeso = document.querySelector('#peso')
+  const inputaltura = document.querySelector('#altura')
+
+  const peso = Number(inputpeso.value)
+  const altura = Number(inputaltura.value)
 
   if (!peso) {
-    setResultado('Peso inválido', false);
+    setResultado('Peso inválido', false)
     return;
   }
-
   if (!altura) {
-    setResultado('Altura inválida', false);
+    setResultado('Altura inválida', false )
     return;
   }
 
-  const imc = getImc(peso, altura);
-  const nivelImc = getNivelImc(imc);
-
-  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
-
-  setResultado(msg, true);
+  const imc = getImc (peso, altura);
+  const nivelimc = getNivelImc (imc);
+  const msg = `Seu IMC é ${imc} (${nivelimc})`
 });
 
+function getImc (peso, altura) {
+  const imc = peso/altura
+  return imc.toFixed(2);
+}
+
 function getNivelImc (imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade nivel 1', 'Obesidade nivel 2', 'Obesidade nivel 3'];
 
   if (imc >= 39.9) return nivel[5];
   if (imc >= 34.9) return nivel[4];
@@ -39,28 +41,16 @@ function getNivelImc (imc) {
   if (imc < 18.5) return nivel[0];
 }
 
-function getImc (peso, altura) {
-  const imc = peso / altura ** 2;
-  return imc.toFixed(2);
-}
-
-function criaP () {
+function criaP() {
   const p = document.createElement('p');
   return p;
 }
 
-function setResultado (msg, isValid) {
+function setResultado (msg, verdadeiro) {
   const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = '';
 
   const p = criaP();
-
-  if (isValid) {
-    p.classList.add('paragrafo-resultado');
-  } else {
-    p.classList.add('bad');
-  }
-
   p.innerHTML = msg;
-  resultado.appendChild(p);
+  resultado.appendChild(p)
+
 }
