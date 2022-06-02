45010ed2f04fe84b5968bbffac4da704cf2f4e13
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-02 18:52:43 -0300

    att

---
+++ imcnovo/assets/js/main.js
@@ -0,0 +1,38 @@
+var peso = window.document.getElementById('peso')
+var altura = window.document.getElementById('altura')
+var res = window.document.getElementById('res')
+
+function check(p, a) {
+
+  if (p.value.length == 0 || a.value.length == 0) {
+    return false
+  } else {
+    return true
+  }
+}
+
+function calculoIMC(p, a){
+  var imc = Number(p.value) / Number(a.value)
+  return imc.toFixed(2)
+}
+
+function nivelIMC(imc) {
+  var nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade nivel 1', 'Obesidade nivel 2', 'Obesidade nivel 3']
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
+}
+
+function calcular() {
+  if (check(peso, altura)) {
+    var imc = calculoIMC(peso, altura)
+    var nivel = nivelIMC(imc)
+    res.innerHTML = `${imc} ${nivel}`
+  } else {
+    res.innerHTML = `[ERRO]. Corrija o valor de Peso ou Altura`
+  }
+} 
