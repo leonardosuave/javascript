bda6137c9c2e47d1a490aa7da792bb3554d0e8c2
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-06 16:35:30 -0300

    Create index.js

---
+++ novasaulas/arrays-atribuicao-via-desistruturacao/index.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+/*
+const primeironumero = numeros[0]
+console.log(primeironumero)
+*/
+
+//vai buscar via desistruturação os primeiros dois indices e atribuir seus valores
+const [primeiroNumero, segundoNumero] = numeros;
+console.log(primeiroNumero, segundoNumero)
