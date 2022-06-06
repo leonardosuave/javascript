54d1bd48a84dae8c597c7ae1ea29a1d3f3477575
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-06 17:02:09 -0300

    Update index.js

--- novasaulas/arrays-atribuicao-via-desistruturacao/index.js
+++ novasaulas/arrays-atribuicao-via-desistruturacao/index.js
@@ -1,9 +1,23 @@
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numeros = [10, 200, 3000, 45, 545, 665, 787, 89, 996];
 /*
 const primeironumero = numeros[0]
 console.log(primeironumero)
 */
 
-//vai buscar via desistruturação os primeiros dois indices e atribuir seus valores
-const [primeiroNumero, segundoNumero] = numeros;
-console.log(primeiroNumero, segundoNumero)
+//vai buscar via desestruturação os primeiros dois indices e atribuir seus valores
+
+//, ...resto -> Faz com que o resto dos numeros do arrays seja atribuido no resto.
+const [um, dois, tres, ...resto] = numeros;
+console.log(um, dois, tres);
+console.log(resto);
+
+/*
+indices:             0          1          2
+                  0  1  2    0  1  2    0  1  2   
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const [ ,[ , ,seis]] = numeros;  OU
+
+const [lista1, lista2, lista3] = numeros;   --> Mais facil
+console.log(lista2[2])
+*/
