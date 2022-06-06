9000a317f4c7cf20710b374f5158d188a12cbed0
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-06 14:11:26 -0300

    Create index.js

---
+++ operadores-ternarios/index.js
@@ -0,0 +1,12 @@
+const pontuacao = 1000
+const nivel = pontuacao >= 1000 ? 'Usuário VIP': 'Usuário Normal'
+console.log(nivel)
+
+/* Método normal
+
+if (pontuação >= 1000) {
+    console.log(`Usuário VIP`)
+}else {
+    console.log(`suário Normal`)
+} 
+*/
