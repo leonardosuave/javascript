48077dc9a7fcb35c934bde3fcabf74b840e56939
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-06 17:51:03 -0300

    Update index.js

--- novasaulas/estrutura-de-repetição-for/index.js
+++ novasaulas/estrutura-de-repetição-for/index.js
@@ -1,3 +1,13 @@
-for (var i = 1; i <=6; i + 2) {
-    console.log(i)
+for (var i = 1; i <=10; i +=1) {
+    const par = i % 2 ===0 ? 'Par' : 'Impar'
+    console.log(i,par)
 }
+
+/* Checagem de número IMPAR ou Par
+{    
+    if(i % 2 == 0){
+        console.log(`${i} é par`)
+    } else {
+        console.log(`${i} é impar`)
+    }
+*/
