642aafce5b3fa12308db0dcfe83cdb1f55ee9fce
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-06 21:14:29 -0300

    att

--- novasaulas/exercici-for/javascript/index.js
+++ novasaulas/exercici-for/javascript/index.js
@@ -2,15 +2,18 @@ const elementos = [
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase2'},
     {tag: 'footer', texto: 'Frase 3'},
-    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'},
 ];
 let h1 = document.querySelector('.container')
 h1.innerHTML = 'HEADER (h1)'
 
 let container = document.querySelector('.container')
-var itemP = document.createElement('div')
-itemP.setAttribute('id', 'parDiv')
-itemP.innerHTML = 'OK Div'
-container.appendChild(itemP)
+var itemdiv = document.createElement('div')
+var itemfooter = document.createElement('footer')
+var itemsection = document.createElement('section')
 
-let
+
+itemdiv.setAttribute('id', 'parDiv')
+itemdiv.innerHTML = 'OK'
+container.appendChild(itemdiv)
+
