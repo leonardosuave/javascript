2e7b03365cd7c421262bb1dbf6afc5a75290647d
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-08 01:09:39 -0300

    Update index.js

--- aulasnovas/setInterval-setTimeout/index.js
+++ aulasnovas/setInterval-setTimeout/index.js
@@ -6,7 +6,17 @@ function mostraHora() {
         hour12: false
     });
 }
-setInterval(function() {console.log(mostraHora())}, 1000) //Por função anonima. (Mais rápido)
+//Por função anonima. (Mais rápido)
+const timer = setInterval(function() {console.log(mostraHora())}, 1000) 
+
+//Para parar o contador
+setTimeout(function() {clearInterval(timer);}, 5000);
+
+//Para exibir uma mensagem quando o contador parar
+setTimeout(function() {
+    console.log('Olá Mundo!');
+}, 5000);
+
 /*Para definir um intervalo de tempo para uma função ser execultada em determinado momento --> setInterval(nome da funcao de repetição, tempo de repetição).
 
 colocar o nome da função que vai ser execultada dentro do parâmetro desta função e o tempo que será execultado separado por virgula --> 1 segundo == 1000 ms */
