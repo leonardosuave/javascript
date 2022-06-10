9f444ce9b985de84bf23bd061317e46daab5075b
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-10 02:13:15 -0300

    Update index.js

--- aulasnovas/Closures/index.js
+++ aulasnovas/Closures/index.js
@@ -10,6 +10,16 @@ console.log(funcao());
 
 //--------------------------------------------------
 
+function retornFuncao3() {
+    return function(nome) {//retorna uma function anonima.
+        return nome; //vai buscar o a var nome em seu vizinho
+    };
+}
+const funcao3 = retornFuncao3();
+console.log(funcao3('Judite'));
+
+//--------------------------------------------------
+
 function retornFuncao1() {
     const nome = 'Tassiane';
     return function() {//retorna uma funcao anonima.
