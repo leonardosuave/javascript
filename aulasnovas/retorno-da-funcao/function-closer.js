7ae7e4085e5d8a67b21a9e8bb01186ecd948f58f
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-10 01:11:30 -0300

    atualização function closure

--- aulasnovas/retorno-da-funcao/function-closer.js
+++ aulasnovas/retorno-da-funcao/function-closer.js
@@ -9,4 +9,6 @@ const duplica = criaMultiplicador(2)
 const triplica = criaMultiplicador(3)
 
 console.log(duplica(2));
-console.log(triplica);
+console.log(triplica(2));
+
+//Const duplica vai executar a function criaMultiplicador(2) e o parâmetro enviado será salvo dentro da function (n) interna. Depois a Const duplica vai receber a function(n) de retorno apos executar criaMultiplicador(2) e o console.log vai execultar a function(n) que foi enviado para duplica e passar o parametro(2) para (n).
