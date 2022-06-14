a7840eaaceb2b58255fd76155dc1a38d7c35a672
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-14 01:20:23 -0300

    Update main.js

--- praticas/calculadora-constructor-functions/js/main.js
+++ praticas/calculadora-constructor-functions/js/main.js
@@ -2,15 +2,33 @@
 function Calculadora () {
   this.display = document.querySelector('.display');
 
+    this.inicia = () => {
+    this.capturaCliques();
+    this.pressionarEnter();
+  }
+
   this.capturaCliques = () => {
     document.addEventListener('click',(e) => {
       const elemento = e.target
       if(elemento.classList.contains('btn-num')) this.addNumDisplay(elemento);
-      if(elemento.classList.contains('btn-clear')) this.clearDisplay(elemento);
-      if(elemento.classList.contains('btn-del')) this.delCharacter(elemento);
-      if(elemento.classList.contains('btn-eq')) this.realizaConta(elemento);
+      if(elemento.classList.contains('btn-clear')) this.clearDisplay();
+      if(elemento.classList.contains('btn-del')) this.delCharacter();
+      if(elemento.classList.contains('btn-eq')) this.realizaConta();
     });
   };
+
+  this.pressionarEnter = () => {
+    document.addEventListener('keypress', (enter) => {
+      if(enter.keyCode === 13) {
+        this.realizaConta();
+      }
+    });
+  }
+
+  this.addNumDisplay = (elemento) => {
+    this.display.value += elemento.innerText;
+    this.display.focus(); //Para tirar o focu do ultimo botão pressionado e assim poder apertar enter para efetuar o calculo.
+  } //pode retirar {}, pois existe so uma linha de codigo com return.
 
   this.clearDisplay = () => {
     this.display.value = '';
@@ -20,13 +38,21 @@ function Calculadora () {
     this.display.value = this.display.value.slice(0, -1);
   }
 
-  this.addNumDisplay = (elemento) => {
-    this.display.value += elemento.innerText;
-  } //pode retirar {}, pois existe so uma linha de codigo com return
+  this.realizaConta = () => {
+    try {
+      const conta = eval(this.display.value);
 
-  this.inicia = () => {
-    this.capturaCliques();
-  }
+      if(!conta) {
+        alert('Conta inválida')
+        return
+      }
+
+      this.display.value = conta;
+    } catch(e) {
+      alert('Conta inválida');
+      return
+    }
+  }  
 }
 
 const calculadora = new Calculadora();
