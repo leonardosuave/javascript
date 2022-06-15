4df61d48ef1e5aedfd7d561805ea50835d3b5cc9
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-15 14:42:07 -0300

    ok

---
+++ Teste/ex1.js
@@ -0,0 +1,16 @@
+function SOMA() {
+    let Indice = 13;
+    let Soma = 0;
+    for(let k = 0; k < Indice; ++k) {
+        Soma = Soma + k
+    }
+    return(Soma)
+};
+console.log(SOMA());
+let k = 0
+let soma = 0
+do {
+    soma = soma + k
+    console
+} while (k < 13) 
+console.log(soma)
