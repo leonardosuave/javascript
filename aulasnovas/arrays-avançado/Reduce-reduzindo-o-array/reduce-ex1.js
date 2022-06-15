9cecdab3d70baf5160d2566e3d00e69eb5af98b7
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-15 01:06:37 -0300

    Reduce - Reduzindo arrays

---
+++ aulasnovas/arrays-avançado/Reduce-reduzindo-o-array/reduce-ex1.js
@@ -0,0 +1,36 @@
+//Utilizada para reduzir o arrays a um unico elemento.
+//É possivel fazer filter() e map() com reduce.
+
+//Some todos os números(reduce)
+//Retorne um array com os pares(filter)
+//Retorne um array com o dobro dos valores(map)
+
+//Total(reduce)
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    acumulador += valor
+    return acumulador;
+}, 0) //sem 0 passa a ser o valor do primeiro índice. 
+console.log(total)
+
+//Pares(filter);
+/*
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    if(valor % 2 === 0) acumulador.push(valor);
+    return acumulador;
+}, [])
+console.log(total) */
+const par = numeros.filter(valor => valor % 2 === 0);
+console.log(par);
+
+//Dobro(map);
+/*
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const total = numeros.reduce(function(acumulador, valor, indice, array) {
+    acumulador.push(valor * 2);
+    return acumulador;
+}, [])
+console.log(total)*/
+const dobro = numeros.map(valor => valor * 2)
+console.log(dobro);
---
+++ aulasnovas/arrays-avançado/Reduce-reduzindo-o-array/reduce-ex2.js
@@ -0,0 +1,15 @@
+//Retorne a pessoa mais velha
+
+const pessoas = [
+    {nome: 'Luiz', idade: 62},
+    {nome: 'Maria', idade: 23},
+    {nome: 'Eduardo', idade: 55},
+    {nome: 'Letícia', idade: 19},
+    {nome: 'Rosana', idade: 64},
+    {nome: 'Wallace', idade: 47},
+];
+const maisVelho = pessoas.reduce(function(acumulador, valor) {
+    if(acumulador.idade > valor.idade) return acumulador;
+    return valor
+});
+console.log(maisVelho);
