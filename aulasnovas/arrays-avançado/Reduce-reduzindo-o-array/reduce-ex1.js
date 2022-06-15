6117bd2b80f8c0bfbc6781507ae91378ed4f2dfa
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-15 01:08:01 -0300

    Update reduce-ex1.js

--- aulasnovas/arrays-avançado/Reduce-reduzindo-o-array/reduce-ex1.js
+++ aulasnovas/arrays-avançado/Reduce-reduzindo-o-array/reduce-ex1.js
@@ -15,7 +15,6 @@ console.log(total)
 
 //Pares(filter);
 /*
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const total = numeros.reduce(function(acumulador, valor, indice, array) {
     if(valor % 2 === 0) acumulador.push(valor);
     return acumulador;
@@ -26,7 +25,6 @@ console.log(par);
 
 //Dobro(map);
 /*
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 const total = numeros.reduce(function(acumulador, valor, indice, array) {
     acumulador.push(valor * 2);
     return acumulador;
