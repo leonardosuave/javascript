4e39efbb39fd2ed3aff765ea9a92474323e22435
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-29 18:11:27 -0300

    Ajax

---
+++ aulasnovas/Promises,Ajax,Fetch e Async-Await/XMLhttpRequest(GET) + Promises/assets/js/main.js
@@ -0,0 +1,36 @@
+//XMLHttpRequest -> Faz requisição de diversos tipos de dados, como:HTML, JSON, XML, texto puro, entre outros.
+
+const request = obj => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET',method, obj.url, true)
+  xhr.send(); //refere-se se estivesse recebendo algum post de formulário.(Neste caso não esta).
+
+  xhr.addEventListener('load', () => {
+
+    //cod entre 200 e 300 - requisição de sucesso.
+    if(xhr.status >= 200 && xhr.status <= 300) {
+
+      //função de callback de sucesso
+      obj.success(xhr.responseText);
+    } else {
+      obj.error({
+        code: xhr
+      })
+    }
+  })
+};
+
+document.addEventListener('click', e => {
+  const elemento = e.target;
+  const tag = elemento.tagName.toLowerCase();
+
+  if(tag === 'a') {
+    e.preventDefault //Para prevenir o evento de carregar a nova página clicada. 
+    carregaPagina(elemento);
+  }
+})
+
+function carregaPagina(elemento){
+  const href = elemento.getAttribute('href');
+  console.log(href)
+}
