120e92692e4ea44dd769932c6b5b73ed234ec0ce
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-30 18:51:23 -0300

    update axios

---
+++ JavaScript full stack - Udemy/Promises,Ajax,Fetch e Async-Await/Fetch API e Axios(JSON)/Axios(Json)/assets/js/main.js
@@ -0,0 +1,32 @@
+/*fetch('pessoas.json')
+    .then(resposta => resposta.json())
+    .then(json => carregaElementosNaPagina(json)); //console.log(json)Aparece um array no console do nav.*/
+axios('pessoas.json')
+    .then(resposta => carregaElementosNaPagina(resposta.data));
+
+function carregaElementosNaPagina(json) {
+    const table = document.createElement('table');
+    
+    for(let pessoa of json) {
+        const tr = document.createElement('tr');
+
+        //Primeira coluna
+       let td = document.createElement('td');
+        td.innerHTML = pessoa.nome;
+        tr.appendChild(td);
+
+        //Segunda coluna
+        td = document.createElement('td');
+        td.innerHTML = pessoa.idade;
+        tr.appendChild(td);
+
+        //Terceira coluna
+        td = document.createElement('td');
+        td.innerHTML = pessoa.salario;
+        tr.appendChild(td);
+       
+        table.appendChild(tr);
+    }
+    let resultado = document.querySelector('.resultado');
+    resultado.appendChild(table)
+}  
