efaed10149155cf0b8d7395f0397c965f34b549e
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-07-04 22:12:23 -0300

    Update index.js

--- JavaScript full stack - Udemy/NodeJS/FSeRecursaoMutua/index.js
+++ JavaScript full stack - Udemy/NodeJS/FSeRecursaoMutua/index.js
@@ -1,8 +1,23 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-//fs.readdir('./') indicaria as pastas que estão na raiz de NodeJS
+//fs.readdir('./') indicaria as pastas que estão na raiz de de JAVASCRIPT
 //path.resolve(--dirname) indica o caminho direto deste arquivo index.js
-fs.readdir(path.resolve(__dirname)) 
-    .then(files => console.log(files))
-    .catch(e => console.log(e));
+
+//fs.readdir(path.resolve(__dirname)) 
+//    .then(files => console.log(files))
+//    .catch(e => console.log(e));
+
+async function readdir(rootDir) {
+    rootDir = rootDir || path.resolve(__dirname); //Utiliza uma das opções.
+    const files =  await fs.readdir(rootDir);
+    walk(files);    
+}
+
+function walk(files) {
+    for(let file of files) {
+        console.log(file)
+    }
+}
+
+readdir('c:\\c\\Users\\Leonardo Suave\\Documents\\Estudos')
