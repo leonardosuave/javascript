12a38e6a2011a90504d017446349552175ef4b47
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-07-06 00:00:09 -0300

    Nodemon

--- JavaScript full stack - Udemy/NodeJS/ExpressIntroducao/server.js
+++ JavaScript full stack - Udemy/NodeJS/ExpressIntroducao/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send(`
     <form action ="/" method="POST">
-    Nome: <input type="text" name="nome">
+    Nome do cliene: <input type="text" name="nome">
     <button> Enviar </button>
     `)
 });
