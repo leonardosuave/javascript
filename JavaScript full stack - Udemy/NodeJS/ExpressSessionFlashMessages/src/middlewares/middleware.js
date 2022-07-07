1458566dcc656d3613673d43afaf27bddf9e68e7
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-07-07 20:44:20 -0300

    att

---
+++ JavaScript full stack - Udemy/NodeJS/ExpressSessionFlashMessages/src/middlewares/middleware.js
@@ -0,0 +1,7 @@
+//Exporta como objeto.
+exports.middlewareGlobal = (req, res, next) => {
+    next()
+};
+
+//Posso passar mais de um middleware aqui por exports.xxx
+//Em server.js fazer require por associação via desestruturação tudo na mesma chave do exemplo, e depois atribuir o uso do express separadamente para cada middle desestruturado.
